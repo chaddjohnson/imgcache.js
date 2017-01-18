48294a7ae186a9ec4a75618f555edfe9f1e63b81
Chad Johnson <chad.d.johnson@gmail.com>
2017-01-17 19:33:34 -0600

    Reverting version patch number change. I hope this fucks nothing up...

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "JS library that stores images locally for offline apps using PhoneGap/Cordova or browsers supporting the new html5 File API",
-  version: "1.1.1",
+  version: "1.1.0",
   git: "https://github.com/chrisben/imgcache.js"
 });
 
